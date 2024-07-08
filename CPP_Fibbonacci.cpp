f73edb1a7d32ce634ffe6e5195700442a48d6d34
SPC <SPC@21-LAB402>
2024-07-08 19:25:39 +0700

    Add project files.

---
+++ CPP_Fibbonacci.cpp
@@ -0,0 +1,28 @@
+// CPP_Fibbonacci.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std; 
+
+int fibo(int n)
+{
+	if (n == 0)
+		return 0;
+	if (n == 1 || n == 2)
+		return 1;
+	else
+		return (fibo(n - 1) + fibo(n - 2));
+}
+		
+
+	int main()
+	{
+		int n = 10;
+		cout << " Fibonacii la: ";
+	for (int i = 0; i < n; i++)
+	{
+		cout << fibo(i) << " ";
+	}
+	return 0; 
+	}
+
